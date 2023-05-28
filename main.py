946cd6216a8047b5171008f81f70889a8478a53c
AromatUspexa <rtomilkin@gmail.com>
2023-05-28 17:05:37 +0600

    Незначительные изменение и без того даунского кода

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import asyncio
-
 import aiohttp
 
 from api import submit_raid
@@ -14,15 +13,25 @@ async def process_token(session: aiohttp.ClientSession, token: str, token_number
 
 
 async def main():
-    with open('tokens.txt', 'r') as file:
-        tokens = [token.strip() for token in file.readlines() if token != "\n"]
-    raid_id = input('Введите ID рейд: ')
     async with aiohttp.ClientSession() as session:
-        tasks = []
-        for i, token in enumerate(tokens):
-            token_number = i + 1
-            tasks.append(asyncio.create_task(process_token(session, token, token_number, raid_id)))
-        await asyncio.gather(*tasks)
+        try:
+            with open('tokens.txt', 'r') as file:
+                tokens = [token.strip() for token in file if token.strip()]
+
+            if not tokens:
+                print("Файл tokens.txt пуст. Пожалуйста, заполните его.")
+                return
+
+            raid_id = input('Введите ID рейда: ')
+
+            tasks = []
+            for i, token in enumerate(tokens, start=1):
+                tasks.append(process_token(session, token, i, raid_id))
+
+            await asyncio.gather(*tasks)
+        except FileNotFoundError:
+            print("Файл tokens.txt не найден.")
+
 
 if __name__ == '__main__':
     asyncio.run(main())
